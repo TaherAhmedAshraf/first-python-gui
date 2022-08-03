20df29ed2ac6079bd532ec941340db1483233243
Taher Ahmed Ashraf <taheirahmedashraf@gmail.com>
2022-08-03 17:56:15 +0600

    First Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is my first GUI made with Python
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+for row in picture:
+    for pixel in row:
+        if pixel:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print('')
